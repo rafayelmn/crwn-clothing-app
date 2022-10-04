0cfc78520f3a0771c0098e2e08b4ab7530100643
rafayel <rafayelmnatsakanyan@gmail.com>
2022-10-04 19:29:15 +0400

    updating categories Context into redux, introduced reselect library, category selector memoization using cretaeSelector method from reselect library

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,18 @@
-export const selectCatagoriesMap = (state) => {
-  console.log('selector fired');
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCatagoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
--- src/store/store.js
+++ src/store/store.js
@@ -9,7 +9,7 @@ const loggerMiddleware = (store) => (next) => (action) => {
   }
 
   console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
+  console.log('payload', action.payload);
   console.log('currentState: ', store.getState());
 
   next(action);
