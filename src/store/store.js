5ffe85fd2c69fc3965077b1c1fac8ae2fc38bd0f
rafayel <rafayelmnatsakanyan@gmail.com>
2022-10-22 22:38:40 +0400

    updating thunk pattern for fetching categories

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 // import { loggerMiddleware } from './middleware/logger';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
@@ -14,9 +15,10 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  thunk,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
