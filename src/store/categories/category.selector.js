c17b6a008c4c86520e3aa351a62c767d02bae339
rafayel <rafayelmnatsakanyan@gmail.com>
2022-10-22 23:17:30 +0400

    using isLoading to render spinner

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -16,3 +16,8 @@ export const selectCatagoriesMap = createSelector(
       return acc;
     }, {})
 );
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
+);
