072bfb543c9109d867fcd983459f0a4bed9afefe
rafayel <rafayelmnatsakanyan@gmail.com>
2022-10-31 05:05:40 +0400

    converting components over to typescript

--- src/components/sign-in-form/sign-in-form.component.tsx
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { FormEvent, ChangeEvent, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { AuthError, AuthErrorCodes } from 'firebase/auth';
 
 import FormInput from '../form-input/form-input.component';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
@@ -28,27 +29,28 @@ const SignInForm = () => {
     dispatch(googleSignInStart());
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
       dispatch(emailSignInStart(email, password));
       resetFormFields();
     } catch (error) {
-      switch (error.code) {
-        case 'auth/wrong-password':
-          alert('incorrect password for email');
-          break;
-        case 'auth/user-not-found':
-          alert('no user associated with this email');
-          break;
-        default:
-          console.log(error);
-      }
+      // switch ((error as AuthError).code) {
+      //   case AuthErrorCodes.INVALID_PASSWORD:
+      //     alert('incorrect password for email');
+      //     break;
+      //   case AuthErrorCodes.USER_DELETED:
+      //     alert('no user associated with this email');
+      //     break;
+      //   default:
+      //     console.log(error);
+      // }
+      console.log('user sign in failes', error);
     }
   };
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
 
     setFormFields({ ...formFields, [name]: value });
